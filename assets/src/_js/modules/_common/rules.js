8fbae680940ed694785c153cb59711dc23e2f3ba
Artyom Kombarov <artyom.kombaroff@gmail.com>
2022-02-06 16:09:58 +0300

    add validation rules

---
+++ assets/src/_js/modules/_common/rules.js
@@ -0,0 +1,43 @@
+const regex = {
+    email: /^(([^<>+()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+};
+
+export default {
+    required: {
+        message: 'Required',
+        method: (el) => {
+            if (el.type === 'checkbox') {
+                return el.checked;
+            }
+            if (el.type === 'radio') {
+                const name = el.name;
+                return el.parentNode.querySelectorAll(`input[name=${name}]:checked`).length > 0;
+            }
+            return el.value !== '';
+        },
+    },
+    email: {
+        message: 'E-mail is wrong',
+        method: (el) => {
+            if (_isRequired(el)) {
+                return regex.email.test(el.value.trim());
+            }
+
+            return regex.email.test(el.value.trim()) || el.value === '';
+        },
+    },
+    phone: {
+        message: 'Phone is wrong',
+        method: (el) => {
+            if (_isRequired(el)) {
+                return el.value.match(/[0-9]/g).length === 11;
+            }
+
+            return el.value.match(/[0-9]/g).length === 11 || el.value === '';
+        },
+    },
+};
+
+function _isRequired(el) {
+    return el.dataset.validate.includes('required');
+}
