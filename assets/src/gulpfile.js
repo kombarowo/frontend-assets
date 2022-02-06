b75a6ffc69dc3980dc9d300355be3b569801f3f2
Artyom Kombarov <artyom.kombaroff@gmail.com>
2022-02-06 17:42:19 +0300

    update gulp

--- assets/src/gulpfile.js
+++ assets/src/gulpfile.js
@@ -1,18 +1,31 @@
+import cssmin from 'gulp-cssnano';
+import dartSass from 'sass';
 import del from 'del';
 import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
 import path from './path.config.js';
-import webpack from 'webpack';
+import prefixer from 'gulp-autoprefixer';
 import webpackConfig from './webpack.config.js';
+import webpackCore from 'webpack';
 import webpackStream from 'webpack-stream';
 
+const scss = gulpSass(dartSass);
 const clean = () => del([path.clean], { force: true });
 
 const compileStyles = () => {
-    return gulp.src(path.src.styles).pipe(gulp.dest(path.dist.styles));
+    return gulp
+        .src(path.src.styles)
+        .pipe(scss({ outputStyle: 'expanded' }))
+        .pipe(prefixer({ overrideBrowserslist: ['last 3 versions'] }))
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.dist.styles));
 };
 
 const compileScripts = () => {
-    return gulp.src(path.src.scripts).pipe(webpackStream(webpackConfig, webpack)).pipe(gulp.dest(path.dist.scripts));
+    return gulp
+        .src(path.src.scripts)
+        .pipe(webpackStream(webpackConfig, webpackCore))
+        .pipe(gulp.dest(path.dist.scripts));
 };
 
 const watch = () => {
--- assets/src/path.config.js
+++ assets/src/path.config.js
@@ -7,11 +7,11 @@ const __dirname = path.dirname(__filename);
 export default {
     dist: {
         scripts: path.resolve(__dirname, '../dist', 'js'),
-        styles: path.resolve(__dirname, '../dist', 'scss'),
+        styles: path.resolve(__dirname, '../dist', 'css'),
     },
     src: {
-        scripts: path.resolve(__dirname, '_js', '*.js'),
-        styles: [path.resolve(__dirname, '_scss', '**', '*.scss')],
+        scripts: [path.resolve(__dirname, '_js', 'main.js'), path.resolve(__dirname, '_js', 'entries', '*.js')],
+        styles: [path.resolve(__dirname, '_scss', 'main.scss'), path.resolve(__dirname, '_scss', 'entries', '*.scss')],
     },
     watch: {
         scripts: path.resolve(__dirname, '_js', '**', '*.js'),
